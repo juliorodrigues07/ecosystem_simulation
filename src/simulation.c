0ecfa1e86be4ae728170d56662b92594d2d64752
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-11-23 14:59:26 -0300

    Fix: Enumerating and assigning valid adjacent positions to move

--- src/simulation.c
+++ src/simulation.c
@@ -1,50 +1,82 @@
 #include "include/simulation.h"
 
+void enumerate_positions (int *adj, int value) 
+{
+    for (int i = 0; i < 4; i++) 
+    {
+        if (adj[i] == -1) 
+        {
+            adj[i] = value;
+            break;
+        }
+    }
+}
+
 void move_rabbits (environment *config, cell **spaces, unsigned int g)
 {
+    int available;
+    int north, south, east, west;
+
     for (int x = 0; x < config->r; x++)
     {
+        north = x - 1;
+        south = x + 1;
+
         for (int y = 0; y < config->c; y++)
         {
-            int possible[4];
-            int available = 0;
-            int north = y - 1;
-            int east = x + 1;
-            int south = y + 1;
-            int west = x - 1;
+            int possible[] = {-1, -1, -1, -1};
+            available = 0;
+            east = y + 1;
+            west = y - 1;
 
             if (config->spaces[x][y].type == 2)
             {
-                if (0 <= north < config->r && config->spaces[x][north].type == 0)
+                if (north >= 0 && north < config->r && config->spaces[north][y].type == 0)
+                {
+                    enumerate_positions(possible, 0);
                     available++;
-                if (0 <= east < config->c && config->spaces[east][y].type == 0)
+                }
+                if (east >= 0 && east < config->c && config->spaces[x][east].type == 0)
+                {
+                    enumerate_positions(possible, 1);
                     available++;
-                if (0 <= south < config->r && config->spaces[x][south].type == 0)
+                }
+                if (south >= 0 && south < config->r && config->spaces[south][y].type == 0)
+                {
+                    enumerate_positions(possible, 2);
                     available++;
-                if (0 <= west < config->c && config->spaces[west][y].type == 0)
+                }
+                if (west >= 0 && west < config->c && config->spaces[x][west].type == 0){
+                    enumerate_positions(possible, 3);
                     available++;
+                }
             }
 
             if (available > 0)
             {
-                int next_cell = (g + x + y) % available;
+                int index = (g + x + y) % available;
+                int next_cell = possible[index];
                 switch (next_cell)
                 {
                     case 0:
-                        spaces[x][north].type = config->spaces[x][y].type;
-                        spaces[x][north].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[north][y].type = config->spaces[x][y].type;
+                        spaces[north][y].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][y].type = 0;
                         break;
                     case 1:
-                        spaces[east][y].type = config->spaces[x][y].type;
-                        spaces[east][y].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][east].type = config->spaces[x][y].type;
+                        spaces[x][east].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][y].type = 0;
                         break;
                     case 2:
-                        spaces[x][south].type = config->spaces[x][y].type;
-                        spaces[x][south].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[south][y].type = config->spaces[x][y].type;
+                        spaces[south][y].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][y].type = 0;
                         break;
                     case 3:
-                        spaces[west][y].type = config->spaces[x][y].type;
-                        spaces[west][y].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][west].type = config->spaces[x][y].type;
+                        spaces[x][west].age_rabbit = config->spaces[x][y].age_rabbit;
+                        spaces[x][y].type = 0;
                         break;
                     default:
                         break;
